1e308e7c48a1054978dafda2b4c07d00fa89bf11
JL-HEIG <jean-luc.blanc2@heig-vd.ch>
2021-03-06 14:41:25 +0100

    add main class picocli

---
+++ main.java
@@ -0,0 +1,19 @@
+/*
+ * Auteurs      : Blanc Jean-Luc, Maillard Mathias, Pozniakoff Lev
+ * Date         : 06.03.2021
+ * Description  : Fichier main permettant de lancer le projet
+ */
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+
+
+
+class Main {
+    @Command(name = "new", description = "TBD")
+    @Command(name = "clean", description = "TBD")
+    @Command(name = "build", description = "TBD")
+    @Command(name = "serve", description = "TBD")
+}
