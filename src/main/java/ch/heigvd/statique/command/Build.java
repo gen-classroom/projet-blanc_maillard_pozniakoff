fb99363b498ba80c27d501ff71dceab4945199bb
JL-HEIG <jean-luc.blanc2@heig-vd.ch>
2021-04-02 11:46:44 +0200

    Finished build method

--- src/main/java/ch/heigvd/statique/command/Build.java
+++ src/main/java/ch/heigvd/statique/command/Build.java
@@ -1,14 +1,45 @@
 package ch.heigvd.statique.command;
 
+import java.io.File;
+import java.io.IOException;
+
 import java.util.concurrent.Callable;
 import picocli.CommandLine.Command;
 
 @Command(name = "build", description = "Build a static site")
 public class Build implements Callable<Integer> {
 
-  @Override public Integer call() {
-    System.out.printf("build");
-    return 1;
+  @Override
+  public Integer call() throws IOException {
+
+    String currentPath = System.getProperty("user.dir") + "/myStaticSite";
+    String buildPath = currentPath + "/build";
+
+    final File myStaticSite = new File(currentPath);
+
+    deleteDirectory(myStaticSite);
+
+    File build = new File(buildPath);
+
+    System.out.println(currentPath);
+    build.mkdirs();
+
+    File htmlFile = new File(buildPath + "/pageRandom.html");
+    htmlFile.createNewFile();
+
+    return 0;
+  }
+
+
+
+  boolean deleteDirectory(File directoryToBeDeleted) {
+    File[] allContents = directoryToBeDeleted.listFiles();
+    if (allContents != null) {
+      for (File file : allContents) {
+        deleteDirectory(file);
+      }
+    }
+    return directoryToBeDeleted.delete();
   }
 
 }
